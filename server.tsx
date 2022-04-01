c0958714d2f62b7de6c3738a67980778182f7c73
organik-inc <luis@organik.express>
2022-04-01 01:05:19 +0000

    First attempt on refactoring the original code into the new core0x module

---
+++ server.tsx
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2022 0xrig by Organik, Inc.
+ * 0xrig Server runs on the port 3000.
+ */
+ var core0x = require("./0xrig/core.tsx");
+ // core0x (Main 0xrig Module)
+ const Fastify = require("fastify");
+ const fastify = Fastify();
+ fastify.register(require('fastify-cors'), {
+    origin: true
+})  
+fastify.register(require('fastify-formbody'));
+fastify.post('/', async function(req, reply){
+    reply.send({
+        method: "POST",
+        data: req.body,
+        message: 'Unsupported method. POST',
+        userAgent: req.headers["user-agent"]
+    })
+})
+
+fastify.get('/', async function(req, reply){
+    reply.send(core0x.defaultRequest(req));
+})
+
+/**
+ * Core Features.
+ */
+
+ fastify.get('/api/:endpoint', async function(req, reply){
+    reply.send(core0x.apiRequest(req));
+    // All API logic is inside this function on core0x.
+ }) 
+/**
+ * Create server on PORT.
+ */
+fastify.listen(core0x.PORT(), (err) => {
+    if(err) throw err;
+    console.log(`🟢 0xrig: Server is online and listening on ${fastify.server.address().port}`)
+})
